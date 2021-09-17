38be50444519236d5691d364ab90e2aaef7ce921
uriegel <uriegel@hotmail.de>
2021-09-17 23:22:21 +0200

    Update some values

--- app/src/main/java/de/uriegel/superfit/room/TrackDao.kt
+++ app/src/main/java/de/uriegel/superfit/room/TrackDao.kt
@@ -10,6 +10,13 @@ interface TrackDao {
     @Insert
     fun insertTrack(track: Track): Long
 
+//    /**
+//     * Updating only price
+//     * By order id
+//     */
+//    @Query("UPDATE Tracks SET order_price=:price WHERE order_id = :id")
+//    fun update(price: Float?, id: Int)
+
     @Query("SELECT * FROM Tracks WHERE _id = :id")
     fun findTrack(id: Int): Track?
 
