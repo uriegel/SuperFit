413ce12f78d8f8b6e95611b92c1d1886f03b50b4
Uwe Riegel <uriegel@hotmail.de>
2023-08-27 00:43:46 +0200

    MapsForge in Kotlin Compose

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "de.uriegel.superfit"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "de.uriegel.superfit2"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("org.mapsforge:mapsforge-core:0.16.0")
+    implementation("org.mapsforge:mapsforge-map:0.16.0")
+    implementation("org.mapsforge:mapsforge-map-reader:0.16.0")
+    implementation("org.mapsforge:mapsforge-themes:0.16.0")
+    implementation("org.mapsforge:mapsforge-map-android:0.16.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.28.0")
+}
