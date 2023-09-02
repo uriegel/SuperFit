d3340a8303ffee8e0cb10c5584a57fb7f206b33e
Uwe Riegel <uriegel@hotmail.de>
2023-09-02 14:00:23 +0200

    launchMultiplePermissionRequest

--- app/src/main/java/de/uriegel/superfit/ui/views/PermissionCheck.kt
+++ app/src/main/java/de/uriegel/superfit/ui/views/PermissionCheck.kt
@@ -1,42 +1,68 @@
 package de.uriegel.superfit.ui.views
 
+import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import de.uriegel.superfit.ui.NavRoutes
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun PermissionCheck(navController: NavController) {
     val context = LocalContext.current
 
-    val requestPermissionLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestMultiplePermissions()
+    var permissionState by remember {
+        mutableStateOf(false)
+    }
+
+    val storagePermissionState = rememberMultiplePermissionsState(
+        listOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION)
     ) {
         val affe = it
-        navController.navigate(NavRoutes.Main.route){ popUpTo(0) }
+        permissionState = true
     }
-    if (context.hasPermissions())
-        navController.navigate(NavRoutes.Main.route){ popUpTo(0) }
-    else
-        SideEffect {
-            requestPermissionLauncher.launch(arrayOf(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE)
-            )
-        }
+
+    var a = permissionState
+    val permis = storagePermissionState.permissions
+    val affff = permis
+
+//    val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+//        // If the user has denied the permission but the rationale can be shown,
+//        // then gently explain why the app requires this permission
+//        "The camera is important for this app. Please grant the permission."
+//    } else {
+//        // If it's the first time the user lands on this feature, or the user
+//        // doesn't want to be asked again for this permission, explain that the
+//        // permission is required
+//        "Camera permission required for this feature to be available. " +
+//                "Please grant the permission"
+//    }
+//    Text(textToShow)
+
+   if (context.hasPermissions())
+       navController.navigate(NavRoutes.Main.route){ popUpTo(0) }
+   else
+       SideEffect {
+           storagePermissionState.launchMultiplePermissionRequest()
+       }
 }
 
 fun Context.hasPermissions() =
-    ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+    ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R)
-            ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
         else
             true
 
